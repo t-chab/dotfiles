2ab652675a661d8ca63a5d71e55ec1b9a2a06244
Thomas Chabaud <tchabaud@chabs.name>
2018-01-05 21:44:45 +0100

    Update completion packages. Add magit.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,10 +75,31 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Everywhere completion
-(use-package ivy
+(use-package counsel
   :config (ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
-  ivy-count-format "%d/%d ")
+  (setq ivy-count-format "%d/%d ")
+  (global-set-key (kbd "C-s") 'swiper)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume))
+
+(use-package counsel-tramp)
+(use-package all-the-icons)
+(use-package all-the-icons-dired
+  :config (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+(use-package all-the-icons-ivy
+  :config (all-the-icons-ivy-setup))
 
 ;; Code completion
 (use-package company
@@ -91,13 +112,18 @@
   (setq company-tooltip-flip-when-above t))
 
 ;; Additional completion packages
+(use-package company-flx
+  :config (company-flx-mode +1))
 (use-package company-ansible)
 (use-package company-dict)
 (use-package company-emoji)
 (use-package company-lua)
+(use-package company-nixos-options)
 (use-package company-quickhelp)
 (use-package company-restclient)
 (use-package company-shell)
+(use-package company-statistics)
+(use-package company-tern)
 (use-package company-try-hard)
 (use-package company-web)
 
@@ -116,6 +142,11 @@
          ("\C-cl" . org-agenda))
   :config (setq org-log-done t))
 
+;; Git integration
+(use-package magit
+  :bind (("C-x g" . magit-status)
+         ("C-x M-g" . magit-dispatch-popup)))
+
 ;; Nice package to automatically disassemble java .class files
 (use-package autodisass-java-bytecode)
 
@@ -125,6 +156,9 @@
 (use-package markdown-toc)
 (use-package json-mode)
 (use-package nix-mode)
+(use-package dockerfile-mode)
+(use-package docker-compose-mode)
+(use-package yaml-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; END - Custom packages
